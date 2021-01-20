b500e316336106efe0a6211bb996d4295d5bd407
Jinyoung-Alice <67137810+Jinyoung-Alice@users.noreply.github.com>
2021-01-20 23:21:03 +0900

    commit: 2021.01.20 / 23:20
    by.Jinyoung

--- src/main/java/com/kh/yourfit/orders/model/dao/ordersDAO.java
+++
@@ -1,9 +0,0 @@
-package com.kh.yourfit.orders.model.dao;
-
-import com.kh.yourfit.orders.model.vo.Orders;
-
-public interface ordersDAO {
-
-	int ordersinsert(Orders orders);
-
-}
--- src/main/java/com/kh/yourfit/orders/model/service/ordersService.java
+++
@@ -1,9 +0,0 @@
-package com.kh.yourfit.orders.model.service;
-
-import com.kh.yourfit.orders.model.vo.Orders;
-
-public interface ordersService {
-
-	int ordersinsert(Orders orders);
-
-}
